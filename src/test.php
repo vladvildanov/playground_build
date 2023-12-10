84ad96bf9ea1ed310432de232f487c65fb8ed9d6
Chayim I. Kirshen <c@kirshen.com>
2023-12-10 15:13:12 +0200

    submoduling

--- src/test.php
+++ src/test.php
@@ -1,3 +1,9 @@
 <?php
 
-echo hello_world_php("Vlad");
+$func = FFI::cdef(
+    "int32_t numbers(int32_t arg, int32_t arg2);",
+    "vendor/chayim/libredis/target/release/libcommands.so"
+);
+$output = $func->numbers(15, 19);
+echo $output;
+?>
